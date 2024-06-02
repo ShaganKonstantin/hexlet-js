efae8a6a5413e51472d7d806ff2e483282d24990
Konstantin Shagan <konstantinshagan@gmail.com>
2024-06-02 17:11:05 +0300

    installed prettier and fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import _ from "lodash"
+import _ from "lodash";
 
-console.log('Hello, Hexlet!')
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet!");
+console.log(_.last(["one", "two"]));
+
+const numbers = [1, 3, 6, 2, -11];
+const result = numbers.sort((a, b) => a - b);
+console.log(result);
